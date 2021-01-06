9dce9561e0895306660b7f2fb2019bfd1bc659db
howardchou0302 <howardchou0302@gmail.com>
2021-01-06 13:00:29 +0800

    mbed: handling disconnection

--- src/mbed/main.cpp
+++ src/mbed/main.cpp
@@ -30,6 +30,8 @@ SpwfSAInterface wifi(MBED_CONF_APP_WIFI_TX, MBED_CONF_APP_WIFI_RX);
 
 MFRC522 *mfrc522 = new MFRC522(D11, D12, D13, D10, A0);
 Servo servo(PWM_OUT);
+bool idSent = false;
+char recv_buffer[1024];
 
 const char *sec2str(nsapi_security_t sec)
 {
@@ -65,47 +67,89 @@ int main()
     printf("Netmask: %s\n", wifi.get_netmask());
     printf("Gateway: %s\n", wifi.get_gateway());
     printf("RSSI: %d\n\n", wifi.get_rssi());
-    TCPSocket socket;
-    SocketAddress addr("192.168.50.124",8888);
-    nsapi_error_t response;
-    // Open a socket on the network interface, and create a TCP connection to addr
-    response = socket.open(&wifi);
-    if (0 != response){
-        printf("Error opening: %d\n", response);
-    }
-    response = socket.connect(addr);
-    if (0 != response){
-        printf("Error connecting: %d\n", response);
-    }
-    socket.set_blocking(1);
-    
     clock_t t = clock();
-    int buffer_sent;
+    int buffer_sent, buffer_recv;
     servo.position(0);
-    RFID_Reader rfidReader(mfrc522);
-    printf("Scanning RFID...\n");
-    printf("cycles per sec: %d\n", CLOCKS_PER_SEC);
     while(1)
     {
-        if(rfidReader.read())
+        TCPSocket socket;
+        SocketAddress addr("192.168.50.124",8888);
+        nsapi_error_t response;
+        // Open a socket on the network interface, and create a TCP connection to addr
+        response = socket.open(&wifi);
+        if (0 != response){
+            printf("Error opening: %d\n", response);
+        }
+        response = socket.connect(addr);
+        if (0 != response){
+            printf("Error connecting: %d\n", response);
+            continue;
+        }
+        else cout << "connected!!" << endl;
+        socket.set_blocking(1);
+        RFID_Reader rfidReader(mfrc522);
+        printf("Scanning RFID...\n");
+        printf("cycles per sec: %d\n", CLOCKS_PER_SEC);
+        buffer_recv = socket.recv(recv_buffer, 1024);
+        cout << recv_buffer << endl;
+        memset(recv_buffer, 0, sizeof(recv_buffer));
+        while(1)
         {
-            printf("New card detected: \n");
-            t = clock();
-            printf("Time: %d\n", t);
-            printf("Card ID: ");
-            char id[2 * rfidReader.getSize()];
-            rfidReader.getCharID(id);
-            for(int i = 0; i < 8; ++i) printf("%c", id[i]);
-            printf("\n");
-            buffer_sent = socket.send(id, 8);
-            servo.position(180);
+            if(idSent){
+                buffer_recv = socket.recv(recv_buffer, 1024);
+                if(buffer_recv < 0)
+                {
+                    cout << "Receiving Error!!!" << endl;
+                    cout << "Disconnecting" << endl;
+                    break;
+                }
+                else if(buffer_recv > 0)
+                {
+                    cout << recv_buffer << endl;
+                    memset(recv_buffer, 0, sizeof(recv_buffer));
+                    servo.position(1);
+                    idSent = false;
+                }
+            }
+            else
+            {
+                if(rfidReader.read())
+                {
+                    printf("New card detected: \n");
+                    t = clock();
+                    printf("Time: %d\n", t);
+                    printf("Card ID: ");
+                    char id[2 * rfidReader.getSize()];
+                    rfidReader.getCharID(id);
+                    for(int i = 0; i < 8; ++i) printf("%c", id[i]);
+                    printf("\n");
+                    buffer_sent = socket.send(id, 8);
+                    if(buffer_sent <= 0)
+                    {
+                        cout << "Sending Error!!!" << endl;
+                        cout << "Disconnecting" << endl;
+                        break;
+                    }
+                    cout << "ID Sending..." << endl;
+                    idSent = true;
+                }
+            }
+            if(float(clock() - t)/CLOCKS_PER_SEC > 5){
+                printf("time expired: %d\n", clock());
+                servo.position(0);
+                t = clock();
+                if(idSent)
+                {
+                    cout << "Sending Timeout !!!" << endl;
+                    cout << "Disconnecting... " << endl;
+                    idSent = false;
+                    break;
+                }
+            }
         }
-        if(float(clock() - t)/CLOCKS_PER_SEC > 5){
-            printf("time expired: %d\n", clock());
-            servo.position(0);
-            t = clock();
-        }
+        float im = servo;
+        printf("%f", im);
+        socket.close();
     }
-    socket.close();
     return 0;
 }
